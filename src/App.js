db0d99088f82eb29f469eab20962c130659f9fda
Gameofpycode <sachinpyamante@gmail.com>
2023-09-21 14:08:32 +0530

    be practical python course page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
+
+
 import "./App.css";
 import Menu from "./component/Menu"
 import Home from "./component/Home"
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+// import ""
+// import 'bootstrap/dist/css/bootstrap.css';
 
 //const local functional component
 const Home = function (props){
     return(
-        <div id="home_page">
+        <div id="home_page col-12">
             {/* <div className="hireme">
             <marquee behavior="scroll" direction="forword"><h1>text hear</h1></marquee>
             </div> */}
             
-            <div className="container">
+            <div className="container col-12">
                 <div className="home_right">
                     <div className="square_up"></div>
                     <div className="square_down"></div>
@@ -16,25 +18,31 @@ const Home = function (props){
                     <div className="home_psd">
                         
                         <div className="img">
-                            <img src="https://iconscout.com/free-logo/html5-42" alt="" width={"100%"} height={"100%"} />
+                            <img src="https://ik.imagekit.io/6xlvknckc/male-programmer-5743382-4846824.webp?updatedAt=1695284193290" alt="" width={"100%"} height={"100%"} />
                         </div>
                         <div className="img">
-                            <img src="https://iconscout.com/free-logo/html5-42" alt="" />
+                            <img src="https://img.freepik.com/premium-photo/development-web-site-with-css-code-creation-style-sheet-improve-website_671359-198.jpg?size=626&ext=jpg&ga=GA1.2.2128482742.1689869008&semt=ais" alt=""
+                            width={"100%"} height={"100%"} />
                         </div>
                         <div className="img">
-                            <img src="" alt="" />
+                            <img src="https://ik.imagekit.io/6xlvknckc/website-development-5984576-4971246.webp?updatedAt=1695284192748" alt=""
+                            width={"100%"} height={"100%"} />
                         </div>
                         <div className="img">
-                            <img src="" alt="" />
+                            <img src="https://ik.imagekit.io/6xlvknckc/3d-javascript-logo-design-free-png.webp?updatedAt=1695283778358" alt="" 
+                            width={"100%"} height={"100%"}/>
                         </div>
                         <div className="img">
-                            <img src="" alt="" />
+                            <img src="https://ik.imagekit.io/6xlvknckc/3d-python-programming-language-logo-free-png.webp?updatedAt=1695283777352" alt=""
+                            width={"100%"} height={"100%"} />
                         </div>
                         <div className="img">
-                            <img src="" alt="" />
+                            <img src="https://ik.imagekit.io/6xlvknckc/web-developer-4788760-3988051%20fly.webp?updatedAt=1695283906103" alt=""
+                            width={"100%"} height={"100%"} />
                         </div>
                         <div className="img">
-                            <img src="" alt="" />
+                            <img src="https://ik.imagekit.io/6xlvknckc/web-developer-4367727-3659200%20fly%202.webp?updatedAt=1695283906100" alt="" 
+                            width={"100%"} height={"100%"}/>
                         </div>
                         
                     </div>
@@ -67,8 +75,53 @@ const Home = function (props){
                    </div>
 
                 </div>
+        <div className="register"> 
+          <div className="card p-2 rounded-1 py-3">
+            <span className="fs-3 text-center">Register For Free Demo Session</span>
+          <div className="row">
+          <div>
+           <label htmlFor='name' className="form-label">Your Name</label>
+                  <input type="text" placeholder='Enter Name' className='form-control'/>
+           </div>
+          <div>
+           <label htmlFor='phone' className="form-label">Mobile Number</label>
+                  <input type="text" placeholder='Phone' className='form-control' />
+           </div>
+            <div>
+            <label htmlFor='Email' className="form-label">Email</label>
+                  <input type="text" placeholder='Phone' className='form-control' />
+ 
             </div>
+            <div>
+            <label htmlFor="course" className="form-label">Select Course</label>
+                  <select className='form-control'>
+                    <option value="mern">MERN</option>
+                    <option value="mern">python full stack</option>
+                    
+                    <option value="mern">Java full stack</option>
+                    <option value="mern">data scince</option>
+                    <option value="mern">cloud computing</option>
+                  </select>
+            </div>
+            <div className="btn1">
+            <button >Join Now</button>
+            </div>
+          </div>
+
+           
+
+            
+              
+            
+           
+             
+    
+            
+          </div> 
         </div>
+        </div> 
+        </div>
+       
     )
 };
 
